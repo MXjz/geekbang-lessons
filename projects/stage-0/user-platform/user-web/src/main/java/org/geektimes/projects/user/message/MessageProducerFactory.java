ccd1ab2630642e62b91524e76635cbaa6a4b9ee2
MXjz <597145896@qq.com>
2021-04-05 10:41:40 +0800

    完善JNDI依赖注入

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/message/MessageProducerFactory.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/message/MessageProducerFactory.java
@@ -1,8 +1,13 @@
 package org.geektimes.projects.user.message;
 
-import javax.jms.*;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Session;
 import javax.naming.Context;
 import javax.naming.Name;
+import javax.naming.RefAddr;
+import javax.naming.Reference;
 import javax.naming.spi.ObjectFactory;
 import java.util.Hashtable;
 
@@ -13,14 +18,23 @@ import java.util.Hashtable;
  */
 public class MessageProducerFactory implements ObjectFactory {
 
-    private String queueName; // 队列名称
-
-    private String connectionFactoryJndiName;
-
     @Override
     public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment) throws Exception {
 
-        ConnectionFactory connectionFactory = (ConnectionFactory) nameCtx.lookup("activemq-factory");
+        /**
+         * 获取Resource中注入的节点数据参考 BeanFactory
+         * @see org.apache.naming.factory.BeanFactory
+         */
+        if (obj == null || !(obj instanceof Reference)) {
+            return null;
+        }
+        Reference ref = (Reference) obj;
+
+        // 获取 queueName
+        String queueName = getAttribute(ref, "queueName");
+        // 获取 connectionFactoryJndiName
+        String conenctionFactoryJndiName = getAttribute(ref, "connectionFactoryJndiName");
+        ConnectionFactory connectionFactory = (ConnectionFactory) nameCtx.lookup(conenctionFactoryJndiName);
 
         Connection connection = connectionFactory.createConnection();
         connection.start();
@@ -29,17 +43,14 @@ public class MessageProducerFactory implements ObjectFactory {
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
         // create the destination
-        Destination destination = session.createQueue("TEST.FOO");
+        Destination destination = session.createQueue(queueName);
 
         // create a MessageProducer from the Session to the topic or queue
         return session.createProducer(destination);
     }
 
-    public void setQueueName(String queueName) {
-        this.queueName = queueName;
-    }
-
-    public void setConnectionFactoryJndiName(String connectionFactoryJndiName) {
-        this.connectionFactoryJndiName = connectionFactoryJndiName;
+    private String getAttribute(Reference ref, String connectionFactoryJndiName) {
+        RefAddr refAddr = ref.get(connectionFactoryJndiName);
+        return refAddr == null ? null : String.valueOf(refAddr.getContent());
     }
 }
